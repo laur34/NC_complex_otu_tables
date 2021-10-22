a7989f06f7a70f913fbbf736a5a9d6682d1116c3
laur34 <lv70xo@gmail.com>
2021-10-22 12:32:37 +0200

    V's script--changed hard-coded variable values into positional arguments

---
+++ v2_negative_controls_correction.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# a script for filtering out reads from OTU tables ...
+# ...based on the number of reads in negative control samples
+
+# script takes as input a pattern to recognize ...
+# ...which samples are biological, and which are negative controls
+
+# by VB@AIM, 04.10.2021
+
+NEG_CTRL_PATTERN="$1"
+OTU_TABLE="$2"
+
+# find maximum of columns with NEG_CTRL_PATTERN in header
+tail -n +2 $OTU_TABLE | \
+	cut -f $(head -1 $OTU_TABLE | \
+	tr "\t" "\n" | \
+	grep -n $NEG_CTRL_PATTERN | \
+	cut -d":" -f1 | \
+	tr "\n" "," | \
+	perl -pe 's/,$/\n/g') | \
+	awk -F $'\t' '{OFS=FS; m=$1; for(i=1;i<=NF;i++) if($i>m) m=$i; print m}' \
+	> neg_ctrls_max_nohead.tsv
+
+# find columns without NEG_CTRL_PATTERN in header ...
+# ...and join with the maximum negative control column
+tail -n +2 $OTU_TABLE | \
+	cut -f $(head -1 $OTU_TABLE | \
+	tr "\t" "\n" | \
+	grep -n "" | \
+	grep -v $NEG_CTRL_PATTERN | \
+	cut -d":" -f1 | \
+	tr "\n" "," | \
+	perl -pe 's/,$/\n/g') \
+	> biol_samples_nohead.tsv
+
+paste -d '\t' <(cut -f1 neg_ctrls_max_nohead.tsv) biol_samples_nohead.tsv \
+	> max_neg_biol_samples.tsv
+
+# remove reads from OTU if number of reads in a sample ...
+# ...is smaller than the maximum negative control read number
+
+awk -F $'\t' '{OFS=FS; m=$1; for(i=3;i<=NF;i++) if($i>m){printf("%d\t", $i)}else{printf("%d\t", "0")}; printf("\n")}' max_neg_biol_samples.tsv \
+	> filtered_biol_samples_nohead.tsv
+
+# get the OTU IDs and the header back
+
+head -1 $OTU_TABLE | \
+	cut -f $(head -1 $OTU_TABLE | \
+	tr "\t" "\n" | \
+	grep -n "" | \
+	grep -v $NEG_CTRL_PATTERN | \
+	cut -d":" -f1 | \
+	tr "\n" "," | \
+	perl -pe 's/,$/\n/g') \
+	> biol_samples_header.tsv
+
+cat biol_samples_header.tsv \
+	<(paste -d $'\t' <(tail -n +2 $OTU_TABLE | cut -f1) filtered_biol_samples_nohead.tsv) \
+	> filtered_${OTU_TABLE}
